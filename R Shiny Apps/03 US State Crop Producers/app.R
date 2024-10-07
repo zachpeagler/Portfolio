0cc3c83edaed966aa6d1034c02bd0d797c61711b
Zach Peagler <123001207+zachpeagler@users.noreply.github.com>
2024-10-06 21:45:50 -0400

    Updated 03 App
    Got column plot working and ordered correctly. Added an input for selecting the number of samples to include on the column plot.

--- R Shiny Apps/03 US State Crop Producers/app.R
+++ R Shiny Apps/03 US State Crop Producers/app.R
@@ -6,13 +6,20 @@ library(tidyverse)
 library(scico)
 library(showtext)
 library(tigris)
+library(sf)
 library(ggthemes)
 library(ggspatial)
+library(DT)
 
 # load data
-LH_state_file <- "C:/Github/Portfolio/R Shiny Apps/03 US State Crop Producers/lh_state_cleaned.csv"
+## test file location
+#LH_state_file <- "C:/Github/Portfolio/R Shiny Apps/03 US State Crop Producers/lh_state_cleaned.csv"
+## deployment file
+LH_state_file <- "lh_state_cleaned.csv"
+## read data
 LH_state <- read.csv(LH_state_file)
 
+# get state information from tigris
 usa <- states()
 ## filter down to the continental 48 states
 usa48 <- usa %>%
@@ -26,7 +33,7 @@ usa48 <- usa %>%
          NAME!="District of Columbia") %>%
   mutate(lon = st_coordinates(st_centroid(.))[,1],
          lat = st_coordinates(st_centroid(.))[,2])
-# order by state name
+## order by state name
 usa48 <- usa48[order(usa48$NAME),]
 
 # merge state geometry info with production info
@@ -44,6 +51,17 @@ showtext_auto()
 seasons <- c("Spring", "Summer", "Fall", "Winter")
 crops <- names(st_drop_geometry(us_st_prod[,47:208]))
 
+# move labels for easier labeling later
+move_labels <- c("Connecticut", "Delaware", "District of Columbia",
+                 "Maryland", "Massachusetts", "New Hampshire", "New Jersey",
+                 "Rhode Island", "Vermont")
+
+move_states <- us_st_prod %>%
+  filter(NAME %in% move_labels) %>%
+  arrange(lat) %>%
+  mutate(xend = -65,
+         yend = seq(min(lat)-5, max(lat)+5, length.out = n()))
+
 # UI
 ui <- fluidPage(
   ### application title
@@ -56,18 +74,20 @@ ui <- fluidPage(
                   choices = crops, selected = "corn"),
       selectInput("palette","Select Palette",
                   choices = p_palettes, selected = "bilbao"),
-      selectInput("fwrap", "Facet Wrap by Season",
-                  choices = c(TRUE, FALSE), selected = FALSE),
-      conditionalPanel(condition = "input.fwrap == 'FALSE'",
+      checkboxInput("fwrap", "Facet Wrap by Season", value=FALSE),
+      conditionalPanel(condition = "input.fwrap == FALSE",
                        selectInput("season", "Select Season",
-                                   choices = seasons, selected = "Fall"))
-    ), #-- end sidebar
+                                   choices = seasons, selected = "Fall")),
+      checkboxInput("labels", "Labels", value=FALSE),
+      sliderInput("nstates", "Top n States", min = 1, max = 50, value = 10)
+                  ), #-- end sidebar
     ### main panel with plot
     mainPanel(
       plotOutput("map"),
-      plotOutput("bar")
-    ) #-- end mainpanel
-  ) #-- end layout
+      plotOutput("bar"),
+      DTOutput("topstates")
+              ) #-- end mainpanel
+    ) #-- end layout
 ) #-- end fluidpage
 
 # server
@@ -78,16 +98,26 @@ server <- function(input, output) {
   Rprod <- reactive({input$product})
   Rpalette <- reactive({input$palette})
   Rfwrap <- reactive({input$fwrap})
-  
+  Rlab <- reactive({input$labels})
+  Rtopn <- reactive({input$nstates})
+  Rtopprod <- reactive({
+    # df to hold top producers
+      us_st_prod %>%
+      arrange(desc(.data[[input$product]])) %>%
+      filter(Season == Rseason()) %>%
+      head(input$nstates)
+  })
+
   # create map
   output$map <- renderPlot({
     
     # filter the data to only be the selected season
     us_st_season <- us_st_prod %>% filter(Season == Rseason())
+    move_states_season <- move_states %>% filter(Season == Rseason())
     
-    if (Rfwrap() == FALSE)
-    # generate plot
-      ggplot(us_st_season, aes(fill=.data[[Rprod()]]))+
+    if (Rfwrap() == FALSE) {
+      # generate plot
+      p <- ggplot(us_st_season, aes(fill=.data[[Rprod()]]))+
         geom_sf(color="black")+
         scale_fill_scico(begin=1, end=0, palette = gettext(Rpalette()))+
         guides(fill = guide_colorbar(title = Rprod()))+
@@ -107,11 +137,26 @@ server <- function(input, output) {
               text = element_text(size=24, family="mont"),
               title = element_text(size=30, family="open", face="bold", lineheight = .5),
               plot.subtitle = element_text(size=24, family="mont", face="italic", lineheight = .5),
-              plot.caption = element_text(size=20, family="mont", face="italic", lineheight = .5))
-    else
-      ggplot(us_st_prod, aes(fill=.data[[Rprod()]]))+
+              plot.caption = element_text(size=20, family="mont", face="italic", lineheight = .5)
+              ) # end theme
+      if (Rlab() == TRUE) {
+        p <- p + geom_label(data = filter(us_st_season, !NAME %in% move_labels),
+                    aes(x = lon, y = lat, label = .data[[Rprod()]]), fill = "grey90", 
+                    family = "mont", size = 5) +
+           geom_label(data = move_states_season,
+                      aes(x = xend, y = yend,
+                          label = paste(STUSPS, .data[[Rprod()]], sep=": ")), fill = "grey90",
+                          family = "mont", size = 5, hjust = 0) +
+           geom_segment(data = move_states_season,
+                        aes(lon, lat, xend = xend, yend = yend),
+                        colour = "grey60", fill=NA, linewidth = 0.3)+
+          xlim(-125, -60)
+      } # end label if statement
+    } # end fwrap if statement
+    else {
+      p <- ggplot(us_st_prod, aes(fill=.data[[Rprod()]]))+
         geom_sf(color="black")+
-        scale_fill_scico(begin=1, end=0, palette = gettext(Rpalette()))+
+        scale_fill_scico(begin=.8, end=0, palette = gettext(Rpalette()))+
         facet_wrap(~Season)+
         guides(fill = guide_colorbar(title = Rprod()))+
         labs(title=paste("Number of farms that produce", Rprod(), "by state", sep =" "),
@@ -121,33 +166,56 @@ server <- function(input, output) {
         annotation_scale(location="bl", text_cex = 2, text_family = "mont")+
         annotation_north_arrow(location="br", height=unit(0.5, "cm"), width=unit(0.5, "cm"))+
         theme_map() +
-        theme(legend.position = "right",
+        theme(legend.position = "topright",
               legend.title.position = "top",
               legend.title = element_text(size=20, family="open"),
               strip.background = element_rect(fill=NA, color=NA),
               text = element_text(size=24, family="mont"),
               title = element_text(size=30, family="open", face="bold", lineheight = .5),
               plot.subtitle = element_text(size=24, family="mont", face="italic", lineheight = .5),
-              plot.caption = element_text(size=20, family="mont", face="italic", lineheight = .5))
-      
-  }) ## end map output
+              plot.caption = element_text(size=20, family="mont", face="italic", lineheight = .5)
+              ) # end theme
+      if (Rlab() == TRUE) {
+          p <- p + geom_label(data = filter(us_st_prod, !NAME %in% move_labels),
+                            aes(x = lon, y = lat, label = .data[[Rprod()]]), fill = "grey90", 
+                            family = "mont", size = 5) +
+                  geom_label(data = move_states,
+                              aes(x = xend, y = yend,
+                                  label = paste(STUSPS, .data[[Rprod()]], sep=": ")), fill = "grey90",
+                                  family = "mont", size = 5, hjust = 0) +
+                  geom_segment(data = move_states,
+                                aes(lon, lat, xend = xend, yend = yend),
+                                colour = "grey60", fill=NA, linewidth = 0.3)+
+                  xlim(-125, -60)
+      } # end label if
+    } # end frwap if
+    print(p)
+  }) ## end map output renderPlot
   
   # barplot
   output$bar <- renderPlot({
-    
-    # df to hold top producers
-    top_prod <- head(us_st_prod[order(Rprod()),] %>% filter(Season == Rseason()), n=10)
-    
-    # plot
-    ggplot(top_prod, aes(x=NAME, y=.data[[Rprod()]]))+
+    # plot  
+    ggplot(Rtopprod(), aes(x= reorder(NAME, -.data[[Rprod()]]), y=.data[[Rprod()]], fill =.data[[Rprod()]]))+
       geom_col()+
-      scale_color_scico(begin=1, end=0, palette = gettext(Rpalette()))+
-      labs(title=paste(Rprod(), "by state", sep =" "))+
+      ylab("Crop Producing Farms")+
+      xlab("State")+
+      labs(title=paste("Number of farms that produce", Rprod(), "by state", sep =" "),
+           subtitle=paste("For the top", Rtopn(), Rprod(), "producing family farms in the continental United States", sep=" "))+
+      scale_fill_scico(begin=.8, end=0, palette = gettext(Rpalette()))+
+      theme_minimal()+
       theme(
       text = element_text(size=24, family="mont"),
-      title = element_text(size=30, family="open", face="bold", lineheight = .5))
-  })
-}
+      title = element_text(size=30, family="open", face="bold", lineheight = .5),
+      plot.subtitle = element_text(size=24, family="mont", face="italic", lineheight = .5),
+      axis.text.x = element_text(size=24, family= "mont", angle=45, hjust=1, vjust=1.1)
+      )
+  }) # end barplot
+  
+  # DT
+  output$topstates <- renderDT({
+    Rtopprod()
+  }) # end topstates output renderDT
+} # end server function
 
 # run app
 shinyApp(ui = ui, server = server)
