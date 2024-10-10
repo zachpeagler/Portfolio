06aa14f9f91553a4e615cdce435d719ab74c1803
Zach Peagler <123001207+zachpeagler@users.noreply.github.com>
2024-10-10 16:45:08 -0400

    01 Update
    Added explainer text around GWalkR visualizations and added GLMs for gsw.
    Updted readme to include more acknowledgements.

--- R Shiny Apps/01 Tomato Inoculants/README.md
+++ R Shiny Apps/01 Tomato Inoculants/README.md
@@ -16,7 +16,7 @@
 
 An R Shiny app created as part of my Master's thesis at Kennesaw State University. It is a comprehensive analysis of four years of tomato data presented in an interactive format.
 
-This largely uses R, the packages dplyr, ggplot2, shiny, showtext, scico, bslib.
+This uses R, the packages tidyverse, shiny, showtext, scico, bslib, MASS, ggpubr, and GWalkR.
 
 ## Hypotheses
 
@@ -33,3 +33,45 @@ This work could not have been possible without my incredible thesis advisor Mari
 The color palette is based on the SLS08 palette by Luis Miguel Maldonado. [Link Here](https://lospec.com/palette-list/slso8)
 
 Thanks to the open source community and all the contributors and maintainers out there. I acknowledge and appreciate your hard work and dedication to open knowledge and tools.
+
+R - R Core Team (2024). _R: A Language and Environment for Statistical
+    Computing_. R Foundation for Statistical Computing, Vienna, Austria.
+    <https://www.R-project.org/>.
+
+Shiny - Chang W, Cheng J, Allaire J, Sievert C, Schloerke B, Xie Y, Allen J,
+  McPherson J, Dipert A, Borges B (2024). _shiny: Web Application
+  Framework for R_. R package version 1.9.1,
+  <https://CRAN.R-project.org/package=shiny>.
+
+Tidyverse - Wickham H, Averick M, Bryan J, Chang W, McGowan LD, François R,
+  Grolemund G, Hayes A, Henry L, Hester J, Kuhn M, Pedersen TL, Miller
+  E, Bache SM, Müller K, Ooms J, Robinson D, Seidel DP, Spinu V,
+  Takahashi K, Vaughan D, Wilke C, Woo K, Yutani H (2019). "Welcome to
+  the tidyverse." _Journal of Open Source Software_, *4*(43), 1686.
+  doi:10.21105/joss.01686 <https://doi.org/10.21105/joss.01686>.
+
+Scico - Pedersen T, Crameri F (2023). _scico: Colour Palettes Based on the
+  Scientific Colour-Maps_. R package version 1.5.0,
+  <https://CRAN.R-project.org/package=scico>.
+
+Showtext - Qiu Y (2024). _showtext: Using Fonts More Easily
+  in R Graphs_. R package version 0.9-7,
+  <https://CRAN.R-project.org/package=showtext>.
+
+Bslib - Sievert C, Cheng J, Aden-Buie G (2024). _bslib: Custom 'Bootstrap'
+  'Sass' Themes for 'shiny' and 'rmarkdown'_. R package version 0.8.0,
+  <https://CRAN.R-project.org/package=bslib>.
+
+MASS - Venables, W. N. & Ripley, B. D. (2002) Modern Applied Statistics with
+  S. Fourth Edition. Springer, New York. ISBN 0-387-95457-0
+
+ggpubr - Kassambara A (2023). _ggpubr: 'ggplot2' Based Publication Ready
+  Plots_. R package version 0.6.0,
+  <https://CRAN.R-project.org/package=ggpubr>.
+
+DT - Xie Y, Cheng J, Tan X (2024). _DT: A Wrapper of the JavaScript
+  Library 'DataTables'_. R package version 0.33,
+  <https://CRAN.R-project.org/package=DT>.
+
+GWalkR - Yu Y (2024). _GWalkR: Interactive Exploratory Data Analysis Tool_. R
+  package version 0.2.0, <https://CRAN.R-project.org/package=GWalkR>.
--- R Shiny Apps/01 Tomato Inoculants/app.R
+++ R Shiny Apps/01 Tomato Inoculants/app.R
@@ -1,5 +1,5 @@
-#### TOMATO INOCULANTS ####
-
+### TOMATO INOCULANTS ###
+##### Setup #####
 #dependencies
 library(shiny)
 library(tidyverse)
@@ -8,9 +8,11 @@ library(showtext)
 library(bslib)
 library(MASS)
 library(ggpubr)
-library(ggridges)
 library(DT)
 library(GWalkR)
+library(rstatix)
+library(lme4)
+library(MuMIn)
 
 # graphical setup
 ## palettes
@@ -24,8 +26,7 @@ font_add_google("Montserrat", family = "mont")
 showtext_auto()
 ## shapes
 four_shapes = c(15,16,17,23)
-
-# load data
+# load data 
 ## li600 data
 Li_data_file <- "TIP24_LI600.csv"
 tfile <- "C:/Github/Portfolio/R Shiny Apps/01 Tomato Inoculants/TIP24_LI600.csv"
@@ -37,16 +38,22 @@ Li_data <- read.csv(tfile, stringsAsFactors = T) %>%
     Row==4~"Germ+Trans",
     TRUE~NA)) %>%
   # filter to leak% < 10 and gsw > 0
-  filter(leak_pct<10, gsw>0) %>%
+  filter(leak_pct<10, gsw > 0) %>%
   mutate(Date = parse_date_time(Date, orders = "mdy"),
          plant_fac = as.factor(paste(Row, Pot)),
          Treatment = factor(Treatment, levels=c("Control", "Germination", 
                                                 "Transplantation", "Germ+Trans"))
         )
-Li_data <- Li_data[,c("Obs.", "Treatment", "Time", "Date", "Row", "Pot", "plant_fac", "gsw", "VPDleaf", "PhiPS2", "rh_s", "Tleaf", "Qamb")]
+Li_data <- Li_data[,c("Treatment", "Time", "Date", "Row", "Pot", "plant_fac", "gsw", "VPDleaf", "PhiPS2", "rh_s", "Tleaf", "Qamb")]
+## Get gsw and PhiPS2 quantiles and interquartile range (IQR)
+Qgsw <- quantile(Li_data$gsw, probs=c(.25, .75), na.rm=FALSE)
+Qps2 <- quantile(Li_data$PhiPS2, probs=c(.25, .75), na.rm=FALSE)
+iqr_gsw <- IQR(Li_data$gsw)
+iqr_ps2 <- IQR(Li_data$PhiPS2)
 ## fruit data
 Fl_data_file <- "TIP24_Fruit.csv"
-Fl_data <- read.csv(Fl_data_file, stringsAsFactors = T) %>%
+tfruitfile <- "C:/Github/Portfolio/R Shiny Apps/01 Tomato Inoculants/TIP24_Fruit.csv"
+Fl_data <- read.csv(tfruitfile, stringsAsFactors = T) %>%
   mutate(Treatment = case_when(
     row==1~"Control",
     row==2~"Transplantation",
@@ -56,8 +63,8 @@ Fl_data <- read.csv(Fl_data_file, stringsAsFactors = T) %>%
     fruit = 1,
     date_analysis = parse_date_time(date_analysis, orders = "mdy"),
     date_harvest = parse_date_time(date_harvest, orders="mdy"),
-    d_harvest = format(date_analysis, format="%d"),
-    d_analysis = format(date_harvest, format="%d"),
+    d_harvest = format(date_harvest, format="%d"),
+    d_analysis = format(date_analysis, format="%d"),
     d_diff = abs(as.integer(d_analysis)-as.integer(d_harvest)),
     plant_fac = as.factor(paste(row, plant)),
     Treatment = factor(Treatment, levels=c("Control","Germination", 
@@ -65,16 +72,24 @@ Fl_data <- read.csv(Fl_data_file, stringsAsFactors = T) %>%
                                            "Germ+Trans")),
     BER = as.factor(BER),
     fungus = as.factor(fungus),
-    cracking = as.factor(cracking)
+    cracking = as.factor(cracking),
+    ripeness = abs(1-(penetrometer/max(na.omit(penetrometer))))
   )
+## Get mass and sugar quantiles and IQR
+Qmass <- quantile(na.omit(Fl_data$mass), probs=c(.25, .75), na.rm=FALSE)
+Qsug <- quantile(na.omit(Fl_data$sugar_avg), probs=c(.25, .75), na.rm=FALSE)
+iqr_mass <- IQR(na.omit(Fl_data$mass))
+iqr_sug <- IQR(na.omit(Fl_data$sugar_avg))
 
-# UI
+###### UI #####
 ui <- navbarPage(title = "Tomato Inoculants",
                  theme = bs_theme(bg = "white", fg=sls8[1], primary = sls8[5],
                                   secondary = sls8[4], base_font = font_google("Open Sans")),
                  sidebar = sidebar(
                    selectInput("palette","Select Color Palette",
-                               choices = p_palettes, selected = "bilbao")
+                               choices = p_palettes, selected = "bilbao"),
+                   checkboxInput("outliers", "Exclude Outliers", FALSE),
+                   checkboxInput("ber", "Exclude Blossom End Rot", FALSE)
                  ), # end global sidebar
   nav_panel("Fruit",
     tabsetPanel(
@@ -96,9 +111,27 @@ ui <- navbarPage(title = "Tomato Inoculants",
       ), # end exploratory tab
       tabPanel("Plots"),
       tabPanel("Statistical Tests"),
-      tabPanel("Data"),
       tabPanel("Tableau-Style Explorer",
-        gwalkrOutput("f_gwalk"))
+        card(card_header("Explanation of Fruit Variables"),
+             card_body(markdown("The tomatoes were grown in 4 rows of 12 pots each, with each row corresponding to a different inoculation treatment. The data table below is formatted in a tidy format with each row corresponding to one fruit and each column representing a variable.<br>
+                                **Treatment** is the inoculation timing of the tomato. Options are Control, Germination, Transplantation, and Germ+Trans. <br>
+                                **row** is the row number of the tomato. (1:4) <br>
+                                **plant** is the pot number of the tomato. (1:12) <br>
+                                **plant_fac** is a combination of *row* and *plant*, and acts as an ID for every individual plant. (1 1: 4 12) <br>
+                                **mass** is the mass in grams of the tomato, measured on an *SCALE NAME*. (~50:~400) <br>
+                                **BER** corresponds to whether or not the tomato has blossom end rot, a disease caused by calcium deficiency that renders the fruit unmarketable. (0,1) <br>
+                                **fungus** corresponds to whether or not the tomato has fungus growing on it. This is common on fruit with BER. (0,1) <br>
+                                **cracking** corresponds to whether or not the tomato has cracks in its skin (0,1) <br>
+                                **penetrometer** corresponds to the force in kilograms it takes to penetrate the flesh of the tomato (~0.5:~4) <br>
+                                **ripeness** is the **penetrometer** value mapped from 0:1 and reversed, so that riper fruit are closer to 1 and unripe fruit are closer to 0. (0:1) <br>
+                                **sugar.1** and **sugar.2** are measurements of the tomato juice's sugar concentration taken on a Fisher BRIX Refractometer (~2:~12) <br>
+                                **sugar_avg** is the average of the two sugar measurements. <br>
+                                **date_harvest** is the date the fruit was harvested (August 2024:October 2024) <br>
+                                **date_analysis** is the date the fruit was analyzed in the lab (August 2024:October 2024) <br>
+                                **d_diff** is the number of days from harvest to analysis. (0:6)"))),
+        gwalkrOutput("f_gwalk"),
+        card(card_body(markdown("For more information on using GWalkR's Tableau-style visualizations please see the author's [Github repo](https://github.com/Kanaries/GWalkR).")))
+      )
     ) # end tab set
   ), # end fruit page
   nav_panel("Fluorescence",
@@ -113,6 +146,7 @@ ui <- navbarPage(title = "Tomato Inoculants",
                      card_body(plotOutput("gsw_cdf")))
               ))),
               card(card_header("One-sample Kolmogorov-Smirnov test results"),
+                   card_body("Add a blurb here about Kolmogorov-Smirnov tests."),
                    card_body(layout_column_wrap(
                      value_box(
                        theme = value_box_theme(bg = sls8[1]),
@@ -124,13 +158,13 @@ ui <- navbarPage(title = "Tomato Inoculants",
                        theme = value_box_theme(bg = sls8[2]),
                        title = "Log-normal p-value",
                        value = textOutput("gsw_lnks"),
-                       p("P-value > 0.05, indicating the data is not significantly different.")
+                       p("P-value < 0.05, indicating the data is significantly different.")
                      ),
                      value_box(
                        theme = value_box_theme(bg = sls8[3]),
                        title = "Gamma p-value",
                        value = textOutput("gsw_gks"),
-                       p("P-value > 0.05, indicating the data is not significantly different.")
+                       p("P-value < 0.05, indicating the data is significantly different.")
                      )
                    ))),
               card(card_body("Based on the above analysis, we can proceed under the assumption
@@ -182,8 +216,8 @@ ui <- navbarPage(title = "Tomato Inoculants",
                     card_body((layout_column_wrap(
                       card(card_header("Stomatal Conductance by Date Across Treatments"),
                            card_body(plotOutput("gsw_date"))),
-                      card(card_header("Scatter Plot"),
-                           card_body(plotOutput("gsw_scatter")))
+                      card(card_header("Stomatal Conductance Boxplot"),
+                           card_body(plotOutput("gsw_box")))
                     )))
                ), # end gsw
                card(card_header("Efficiency of Photosystem II (PhiPS2)",
@@ -196,23 +230,100 @@ ui <- navbarPage(title = "Tomato Inoculants",
                     )))
                ) # end phips2
                ),
-      tabPanel("Statistical Tests"),
-      tabPanel("Data"),
+      tabPanel("Statistical Tests",
+               card(card_header("Stomatal Conductance (gsw)"),
+                    card_body("Based on the exploratory analysis, either a gamma or
+                              log-normal distribution are appropriate for gsw. To determine the
+                              effect Treatment has on gsw, we'll use a generalized linear model with
+                              gsw as our response variable, Treatment as a predictor, relative humidity
+                              as a fixed effect, and plant as a random effect, with a gamma link function.
+                              I'm unsure of whether to use a log or gamma family link function, so I'll
+                              do both and compare them."),
+                    card_body(layout_column_wrap(
+                      card(card_header("GLM with Gamma family and log link"),
+                         card_body(verbatimTextOutput("gsw_glm_gamma"), max_height= 500),
+                         card_body(layout_column_wrap(
+                           card(card_header("Pseudo-R2"),
+                                card_body(verbatimTextOutput("gsw_glm_gamma_r2"))),
+                           value_box(title = "AIC", value = textOutput("gsw_glm_gamma_AIC"))
+                          ))
+                        ),
+                      card(card_header("GLM with gaussian family and log link"),
+                           card_body(verbatimTextOutput("gsw_glm_log"), max_height= 500),
+                           card_body(layout_column_wrap(
+                             card(card_header("Pseudo-R2"),
+                                  card_body(verbatimTextOutput("gsw_glm_log_r2"))),
+                             value_box(title = "AIC", value = textOutput("gsw_glm_log_AIC"))
+                           ))
+                          )
+                      ))) # end gsw card
+               ), # end statistical tests panel
       tabPanel("Tableau-Style Explorer",
-               gwalkrOutput("li_gwalk"))
+               card(card_header("Explanation of Fluorescence Variables"),
+                    card_body(markdown("Fluorescence measurements were taken biweekly with a LI-COR LI-600 over the course of the trial. Data is presented in a tidy format with each row representing a single observation and each column representing a variable. <br>
+                                **Treatment** is the inoculation timing of the tomato. Options are Control, Germination, Transplantation, and Germ+Trans. <br>
+                                **Time** is the time at which the measurement was taken. <br>
+                                **Date** is the date at which the measurement was taken. <br>
+                                **Row** is the row number of the tomato. (1:4) <br>
+                                **Pot** is the pot number of the tomato. (1:12) <br>
+                                **plant_fac** is a combination of *Row* and *Pot*, and acts as an ID for every individual plant. (1 1: 4 12) <br>
+                                **gsw** is the stomatal conductance (mol m-2 s-1) of the leaf. <br>
+                                **VPDleaf** is the vapor pressure deficit (add units) of the leaf. <br>
+                                **PhiPS2** is the efficiency of photosystem II of the leaf. It is unitless. (0:1) <br>
+                                **rh_s** is the relative humidity (add units) of the leaf. <br>
+                                **Tleaf** is the temperature (C) of the leaf. <br>
+                                **Qamb** is the ambient light level (add units) at the time of the observation.
+                                ")),
+                    gwalkrOutput("li_gwalk"),
+                    card_body(markdown("For more information on using GWalkR's Tableau-style visualizations please see the author's [Github repo](https://github.com/Kanaries/GWalkR)."))
+                    )
+              )
     ) # end tab set
   ), # end fluorescence page
+  nav_spacer(),
+  nav_item(tags$a("Github", href = "https://github.com/zachpeagler/Portfolio/tree/main/R%20Shiny%20Apps/01%20Tomato%20Inoculants"))
 ) # end ui
 
-##   nav_item(tags$a("Github", href = "https://github.com/zachpeagler/Portfolio/tree/main/R%20Shiny%20Apps/01%20Tomato%20Inoculants"))
-# server
+##### server #####
 server <- function(input, output) {
 ## Reactive expressions
   Rpalette <- reactive({input$palette})
+### Reactive dataframes
+  RLi_data <- reactive({
+    x <- Li_data
+    if (input$outliers == TRUE) {
+      x <- subset(x, gsw > (Qgsw[1]-1.5*iqr_gsw) &
+                gsw < (Qgsw[2]+1.5*iqr_gsw) &
+                PhiPS2 > (Qps2[1]-1.5*iqr_ps2) &
+                PhiPS2 < (Qps2[2]+1.5*iqr_ps2))
+    }
+    return(x)
+  })
+  RFl_data <- reactive ({
+    f <- Fl_data
+    if (input$outliers == TRUE) {
+      f <- subset(f, mass > (Qmass[1]-1.5*iqr_mass) &
+             mass < (Qmass[2]+1.5*iqr_mass) &
+             sugar_avg > (Qsug[1]-1.5*iqr_sug) &
+             sugar_avg < (Qsug[2]+1.5*iqr_sug))
+    }
+    if (input$ber == TRUE){
+      f <- subset(f, BER == 0)
+    }
+    return(f)
+  })
+### Reactive GLMs 
+  Rgsw_glm_gamma <- reactive({
+    glmer(gsw ~ Treatment + rh_s + (1 | plant_fac),
+          data = RLi_data(), family = Gamma(link = "log"))
+  })
+  Rgsw_glm_log <- reactive({
+    glmer(gsw ~ Treatment + rh_s + (1 | plant_fac),
+          data = RLi_data(), family = gaussian(link = "log"))
+  })
 ## Fruit
 ### Exploratory
 #### sugar
-
 #### CDF
 
   
@@ -391,9 +502,18 @@ server <- function(input, output) {
         axis.title = element_text(size=24, family = "mont", face= "bold"),
       )
   })
+
+## Statistical outputs
+  output$gsw_glm_gamma <- renderPrint({ summary(Rgsw_glm_gamma())})
+  output$gsw_glm_gamma_r2 <- renderPrint({ r.squaredGLMM(Rgsw_glm_gamma())})
+  output$gsw_glm_gamma_AIC <- renderText({ AIC(Rgsw_glm_gamma())})
+  output$gsw_glm_log <- renderPrint({ summary(Rgsw_glm_log())})
+  output$gsw_glm_log_r2 <- renderPrint({ r.squaredGLMM(Rgsw_glm_log())})
+  output$gsw_glm_log_AIC <- renderText({ AIC(Rgsw_glm_log())})
   
-  
-  output$li_gwalk <- renderGwalkr(gwalkr(Li_data))
+## GWalkR outputs
+  output$li_gwalk <- renderGwalkr({gwalkr(na.omit(RLi_data()))})
+  output$f_gwalk <- renderGwalkr({gwalkr(na.omit(RFl_data()[,c(13,1,2,18,3:7,19,8:12,17)]))})
 }
 
 # run app
